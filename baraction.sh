69f2958da58a325edb679a77ad49df352931bf51
kbc8090 <63003083+kbc8090@users.noreply.github.com>
2020-06-03 15:41:43 -0400

    Add files via upload

--- baraction.sh
+++ baraction.sh
@@ -14,7 +14,7 @@ dte() {
 
 ## RAM
 mem() {
-	mem=`free | awk '/Mem/ {printf "%d MB/%d MB\n", $3 / 1024.0, $2 / 1024.0 }'`
+	mem="$(free | awk '/Mem/ {printf "%d MB/%d MB\n", $3 / 1024.0, $2 / 1024.0 }')"
 	echo -e $mem
 }
 
@@ -25,16 +25,16 @@ vol() {
 	echo -e "${vol}%"
 }
 
-##CREATE THE BAR OUTPUT
-##LOOPS WEATHER AND PACMAN UPDATES EVERY 25MINS, EVERYTHING ELSE EVERY 3 SECONDS
+## CREATE THE BAR OUTPUT
+## LOOPS WEATHER AND PACMAN UPDATES EVERY 25MINS, EVERYTHING ELSE EVERY 3 SECONDS
 I=0
 SLEEP_SEC=3
 while :; do
 	if [ $I -eq 0 ]; then
 		PACUPDATE=$(checkupdates | wc -l)
-		WEATHER=$(curl -s 'wttr.in/Gainesville?format=1&u')
+		WEATHER=$(curl -s 'wttr.in/Gainesville?format=1&u' | sed 's/ //g' | sed 's/+//g')
 		WTEXT=$(grep -o "[0-9].*" <<< "$WEATHER")
-		WICON=$(awk '{print $1}' <<< "$WEATHER")
+		WICON=${WEATHER:0:1}
 	fi
 	echo -e "+@fg=6;+@fn=1;$WICON+@fn=0; $WTEXT +@fg=0;| +@fg=1;+@fn=1;🔁+@fn=0; Updates: +@fg=4;$PACUPDATE+@fg=1; [$(kern)] +@fg=0;| +@fg=2;+@fn=1;💾+@fn=0; $(mem) +@fg=0;|+@fg=4; +@fn=1;🔊+@fn=0; $(vol) +@fg=0;| +@fg=5;+@fn=1;🗓+@fn=0; $(dte)"
 	I=$(( ( ${I} + 1 ) % 500 ))
